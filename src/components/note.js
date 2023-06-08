31e095c3c6dfe5f564566deec8bb4a910f716ba8
jai <jaigaur.imp210@gmail.com>
2023-06-08 14:48:06 +0530

    commit

---
+++ src/components/note.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(props){
+    return <note>
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
+    </note>
+    
+   
+}
+
+export default Note;
